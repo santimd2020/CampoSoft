38f6f57b84dd7de89ebed9a64b913a9f8e382787
Santiago Muñoz <60823580+santimd2020@users.noreply.github.com>
2022-09-26 18:41:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def index():
- return "Funciona!"
+from app.app import app
+
 if __name__ == '__main__':
- app.run()
+    app.run(debug=True, port=8080)
